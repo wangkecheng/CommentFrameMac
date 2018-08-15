3e1139a7c18285727547442a337417555038ce5d
王帅 <670894753@qq.com>
2018-08-15 09:14:05 +0800

    no message

--- CommentFrameMac/AppDelegate.m
+++ CommentFrameMac/AppDelegate.m
@@ -18,11 +18,8 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     
-     _homeWC = [[HomeWC alloc]initWithWindowNibName:@"HomeVC"];
-    //让显示的位置居于屏幕的中心
-//    [[_homeVC window] center];
-    //前置显示窗口
-    [_homeWC.window orderFront:nil];
+     _homeWC = [[HomeWC alloc]initWithWindowNibName:@"HomeVC"]; 
+    [_homeWC.window orderFront:nil]; //前置显示窗口
 }
 
 
--- CommentFrameMac/视图/首页/HomeWC.m
+++ CommentFrameMac/视图/首页/HomeWC.m
@@ -17,7 +17,8 @@
 - (void)windowDidLoad {
     [super windowDidLoad];
     
-    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    //让显示的位置居于屏幕的中心
+    //[[_homeVC window] center]; 
 }
 
 @end
